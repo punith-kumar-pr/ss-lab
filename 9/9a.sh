38ea660f293e37b3ce2e9dcc985ef574a952775e
Punith Kumar P R <94525391+punith-kumar-pr@users.noreply.github.com>
2023-06-26 19:07:40 +0530

    Resolved error in 9a.sh program

--- 9/9a.sh
+++ 9/9a.sh
@@ -4,7 +4,7 @@ echo "Invalid input!!!"
 else
 p1=`ls -l $1|cut -d " " -f1`
 p2=`ls -l $2|cut -d " " -f1`
-if [ $p1 == $p2 ]
+if [ $p1 = $p2 ]
 then
 echo "the file permissions are same and it is : "
 echo "$p1"
